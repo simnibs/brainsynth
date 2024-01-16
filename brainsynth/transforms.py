1c77284b1866b65200ea6e7b1d02ddf1632a1f3d
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2024-01-16 10:07:27 +0100

    Created onehot enc. transform

--- brainsynth/transforms.py
+++ brainsynth/transforms.py
@@ -1,14 +1,25 @@
 import monai
 import torch
 
+
 class Reindex(monai.transforms.Transform):
-    def __init__(self, labels: torch.Tensor):
-        """Reindex data, e.g., [1,3,5,7] to [0,1,2,3].
-        """
+    def __init__(self, labels: torch.IntTensor):
+        """Reindex data, e.g., [1,3,5,7] to [0,1,2,3]."""
+        kwargs = dict(dtype=torch.int, device=labels.device)
         labels = labels.sort().values
-        self.reindexer = torch.zeros(labels.amax() + 1, dtype=torch.int)
-        self.reindexer[labels] = torch.arange(len(labels), dtype=torch.int)
+        self.reindexer = torch.zeros(labels.amax() + 1, **kwargs)
+        self.reindexer[labels] = torch.arange(len(labels), **kwargs)
 
     def __call__(self, data: torch.IntTensor):
         return self.reindexer[data]
 
+
+class AsDiscreteWithReindex(monai.transforms.Compose):
+    def __init__(self, labels) -> None:
+        super().__init__(
+            [
+                monai.transforms.EnsureType(dtype=torch.int),
+                Reindex(labels),
+                monai.transforms.AsDiscrete(to_onehot=len(labels)),
+            ]
+        )
