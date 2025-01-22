d2af497b6bc313a4fec374669f4365901eaac67f
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2025-01-22 15:43:04 +0100

    added generic dataset for inference

--- brainsynth/dataset.py
+++ brainsynth/dataset.py
@@ -5,11 +5,10 @@ import nibabel as nib
 import numpy as np
 import torch
 
-import brainsynth.constants
 from brainsynth.constants import IMAGE, SURFACE
-
 from brainsynth.config import DatasetConfig, SynthesizerConfig, XDatasetConfig
 from brainsynth.synthesizer import Synthesizer
+from brainsynth.utilities import apply_affine
 
 def atleast_4d(tensor):
     return atleast_4d(tensor[None]) if tensor.ndim < 4 else tensor
@@ -18,22 +17,33 @@ def atleast_4d(tensor):
 def load_dataset_subjects(filename):
     return np.atleast_1d(np.genfromtxt(filename, dtype="str")).tolist()
 
-def _load_image(filename, dtype, transform: Callable | None = None, return_affine: bool = False):
+
+def _load_image(
+    image: nib.Nifti1Image | Path | str, dtype, transform: Callable | None = None, return_affine: bool = False
+):
     # Images seem to be (x,y,z,c) or (x,y,z) but we want (c,x,y,z)
-    img = nib.load(filename)
+    if isinstance(image, nib.Nifti1Image):
+        img = image
+    else:
+        img = nib.load(image)
     data = torch.tensor(img.dataobj[:])
     data = data if transform is None else transform(data)
     data = data.to(dtype=dtype)
 
     if data.ndim < 3:
-        raise ValueError(f"Image {filename} has less than three dimensions (shape is {data.shape})")
-    elif data.ndim == 3: # (x,y,z) -> (c,x,y,z)
+        raise ValueError(
+            f"Image {filename} has less than three dimensions (shape is {data.shape})"
+        )
+    elif data.ndim == 3:  # (x,y,z) -> (c,x,y,z)
         data = atleast_4d(data)
-    elif data.ndim == 4: # (x,y,z,c) -> (c,x,y,z)
-        data = data.permute((3,0,1,2)).contiguous()
+    elif data.ndim == 4:  # (x,y,z,c) -> (c,x,y,z)
+        data = data.permute((3, 0, 1, 2)).contiguous()
     elif data.ndim > 4:
-        raise ValueError(f"Image {filename} has more than four dimensions (shape is {data.shape})")
-    return (data, torch.tensor(img.affine)) if return_affine else data
+        raise ValueError(
+            f"Image {filename} has more than four dimensions (shape is {data.shape})"
+        )
+    return (data, torch.tensor(img.affine, dtype=torch.float)) if return_affine else data
+
 
 class SynthesizedDataset(torch.utils.data.Dataset):
     def __init__(
@@ -48,7 +58,7 @@ class SynthesizedDataset(torch.utils.data.Dataset):
         target_surface: dict | None = {},
         initial_surface: dict | None = {},
         randomize_hemisphere: bool = False,
-        xdataset: None | XDatasetConfig = None, # or XDataset
+        xdataset: None | XDatasetConfig = None,  # or XDataset
     ):
         """
 
@@ -134,21 +144,27 @@ class SynthesizedDataset(torch.utils.data.Dataset):
 
         # Subjects
         self.subjects = (
-            load_dataset_subjects(subjects) if isinstance(subjects, (Path, str)) else subjects
+            load_dataset_subjects(subjects)
+            if isinstance(subjects, (Path, str))
+            else subjects
         )
         self.n_subjects = len(self.subjects)
 
         # Filename generators
         match self.ds_structure:
             case "flat":
+
                 def get_image_filename(subject, image):
                     return self.ds_dir / f"{self.name}.{subject}.{image}"
+
                 def get_surface_filename(subject, surface):
                     # return self.ds_dir / f"{self.name}.{subject}.{surface}"
                     return self.ds_dir / f"{self.name}.{subject}.surf_dir" / surface
             case "tree":
+
                 def get_image_filename(subject, image):
                     return self.ds_dir / self.name / subject / image
+
                 def get_surface_filename(subject, surface):
                     return self.ds_dir / self.name / subject / surface
 
@@ -158,7 +174,9 @@ class SynthesizedDataset(torch.utils.data.Dataset):
     def _initialize_image_settings(self, images):
         self.images = list(IMAGE.images._fields) if images is None else list(images)
 
-    def _initialize_surface_settings(self, target_surface_kwargs, initial_surface_kwargs):
+    def _initialize_surface_settings(
+        self, target_surface_kwargs, initial_surface_kwargs
+    ):
         # Target surface
         if target_surface_kwargs is None:
             self.target_surface_files = {}
@@ -205,7 +223,7 @@ class SynthesizedDataset(torch.utils.data.Dataset):
                         assert len(v) == 1
                         initial_surface_kwargs["types"] = v[0]
                 kwargs |= initial_surface_kwargs
-            assert all(i==j for i,j in zip(kwargs["hemispheres"], self.hemispheres))
+            assert all(i == j for i, j in zip(kwargs["hemispheres"], self.hemispheres))
             self.initial_surface_kwargs = kwargs
             self.initial_surface_files = SURFACE.get_files(**kwargs)
 
@@ -213,7 +231,7 @@ class SynthesizedDataset(torch.utils.data.Dataset):
         return len(self.subjects)
 
     def __getitem__(self, idx):
-        return self.load_data(self.subjects[idx])#, self.subjects[idx], self.name
+        return self.load_data(self.subjects[idx])  # , self.subjects[idx], self.name
 
     def load_data(self, subject):
         images = self.load_images(subject)
@@ -221,10 +239,10 @@ class SynthesizedDataset(torch.utils.data.Dataset):
 
         if self.xdataset is not None:
             # select a random subject from xdataset
-            idx = torch.randint(0, len(self.xdataset), (1, ))
+            idx = torch.randint(0, len(self.xdataset), (1,))
             ximages = self.xdataset[idx]
             # to distinguish from "self"
-            images |= {f"other:{k}":v for k,v in ximages.items()}
+            images |= {f"other:{k}": v for k, v in ximages.items()}
 
         if self.synthesizer is None:
             return images, surfaces, initial_vertices
@@ -246,9 +264,11 @@ class SynthesizedDataset(torch.utils.data.Dataset):
                         # NOTE
                         # Invert the defacing mask so that *valid* voxels are
                         # true
-                        images[img.defacingmask] = ~ _load_image(fm, mask.dtype)
+                        images[img.defacingmask] = ~_load_image(fm, mask.dtype)
                     elif self.load_mask == "force":
-                        images[img.defacingmask] = torch.ones(images[image].shape, dtype=mask.dtype)
+                        images[img.defacingmask] = torch.ones(
+                            images[image].shape, dtype=mask.dtype
+                        )
         return images
 
     def load_surfaces(self, subject):
@@ -294,12 +314,11 @@ class SynthesizedDataset(torch.utils.data.Dataset):
             for h in hemi
         }
 
-
     def _load_initial_surfaces(self, subject, hemi):
         if (t := self.initial_surface_kwargs["types"]) is not None:
             return {
                 h: torch.load(
-                    self.get_surface_filename(subject, self.initial_surface_files[h,t])
+                    self.get_surface_filename(subject, self.initial_surface_files[h, t])
                 )
                 for h in hemi
             }
@@ -359,7 +378,6 @@ class XDataset(torch.utils.data.Dataset):
         images = ["mni152_nonlin_backward", "mni152_nonlin_forward"]
         self._initialize_image_settings(images)
 
-
     def _initialize_io_settings(self, ds_dir, name, ds_structure, subjects):
         self.ds_dir = Path(ds_dir)
         self.name = name
@@ -367,16 +385,20 @@ class XDataset(torch.utils.data.Dataset):
 
         # Subjects
         self.subjects = (
-            load_dataset_subjects(subjects) if isinstance(subjects, (Path, str)) else subjects
+            load_dataset_subjects(subjects)
+            if isinstance(subjects, (Path, str))
+            else subjects
         )
         self.n_subjects = len(self.subjects)
 
         # Filename generators
         match self.ds_structure:
             case "flat":
+
                 def get_image_filename(subject, image):
                     return self.ds_dir / f"{self.name}.{subject}.{image}"
             case "tree":
+
                 def get_image_filename(subject, image):
                     return self.ds_dir / self.name / subject / image
 
@@ -398,6 +420,116 @@ class XDataset(torch.utils.data.Dataset):
             fi = self.get_image_filename(subject, img.filename)
             images[image] = _load_image(fi, img.dtype, img.transform)
         return images
+
+class GenericDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        images: list | tuple[Path, str],
+        mni_transform: list | tuple[Path, str],
+        mni_direction: str = "mni2sub",
+        mni_space: str = "mni152",
+        hemi: str | None = None,
+        nv_template: int = 62,
+    ):
+        """Dataset formed from a list of images and transformations.
+
+
+        Parameters
+        ----------
+
+        """
+        assert len(images) == len(mni_transform)
+        assert mni_space in ("mni152", "mni305")
+        assert mni_direction in {"mni2sub", "sub2mni"}
+        self.hemi = (
+            tuple(
+                hemi,
+            )
+            if isinstance(hemi, str)
+            else SURFACE.hemispheres
+        )
+
+        self.images = images
+        self.mni_transform = mni_transform
+        self.mni_space = mni_space
+        self.mni_direction = mni_direction
+
+        self.template = {}
+        self.template_meta = {}
+
+
+
+        # FSAVERAGE
+        # for h in self.hemi:
+        #     v, _, m = nib.freesurfer.read_geometry(
+        #         brainsynth.resources_dir / f"{h}.white.smooth", read_metadata=True
+        #     )
+        #     self.template[h] = torch.tensor(v[:nv_template].astype(np.float32))
+        #     self.template_meta[h] = m
+
+        # TOPOFIT
+        v, _, m = nib.freesurfer.read_geometry(
+            brainsynth.resources_dir / "cortex-int-lh.srf", read_metadata=True
+        )
+        flip_x = np.array([-1, 1, 1])
+        for h in self.hemi:
+            if h == "lh":
+                self.template[h] = torch.tensor(v[:nv_template].astype(np.float32))
+            elif h == "rh":
+                self.template[h] = torch.tensor((v[:nv_template]*flip_x).astype(np.float32))
+            self.template_meta[h] = m
+
+
+
+        # (8) from https://surfer.nmr.mgh.harvard.edu/fswiki/CoordinateSystems
+        mni305_to_mni152 = torch.tensor(
+            [
+                [0.9975, -0.0073, 0.0176, -0.0429],
+                [0.0146, 1.00090003, -0.0024, 1.54960001],
+                [-0.013, -0.0093, 0.9971, 1.18400002],
+                [0.0, 0.0, 0.0, 1.0],
+            ]
+        )
+
+        match self.mni_space:
+            case "mni152":
+                # the template vertices are in mni305 so add a transformation
+                # from mni305 to mni152
+                self._preprocess_mni_transform = lambda t: self._mni_to_sub(t) @ mni305_to_mni152
+            case "mni305":
+                self._preprocess_mni_transform = lambda t: self._mni_to_sub(t)
+
+    def _mni_to_sub(self, t):
+        return torch.linalg.inv(t) if self.mni_direction == "sub2mni" else t
+
+    def _load_mni_transform(self, index):
+        return np.loadtxt(self.mni_transform[index])
+
+    def prepare_initial_vertices(self, trans, vox2mri):
+        trans = torch.linalg.inv(vox2mri) @ trans
+        return {k: apply_affine(trans, v) for k, v in self.template.items()}
+
+    def load_initial_vertices(self, index, vox2mri):
+        trans = self._load_mni_transform(index)
+        trans = torch.tensor(trans, dtype=torch.float)
+        trans = self._preprocess_mni_transform(trans)
+        return self.prepare_initial_vertices(trans, vox2mri)
+
+    def preprocess_image(self, img):
+        return img
+
+    def load_image(self, index):
+        img = nib.load(self.images[index])
+        img = self.preprocess_image(img)
+        return _load_image(img, torch.float, return_affine=True)
+
+    def __len__(self):
+        return len(self.subjects)
+
+    def __getitem__(self, index):
+        image, vox2mri = self.load_image(index)
+        template = self.load_initial_vertices(index, vox2mri)
+        return image, template, vox2mri
 
 
 def make_dataloader(
@@ -424,6 +556,7 @@ def make_dataloader(
         )
     return torch.utils.data.DataLoader(dataset, **kwargs)
 
+
 def concatenate_datasets(
     dataset_config: DatasetConfig,
     dataset_class: SynthesizedDataset = SynthesizedDataset,
@@ -431,11 +564,13 @@ def concatenate_datasets(
     return torch.utils.data.ConcatDataset(
         [dataset_class(**kw) for kw in dataset_config.dataset_kwargs.values()]
     )
+
+
 def setup_dataloader(
     dataset_config: DatasetConfig,
     dataloader_kwargs: dict | None = None,
     separate_datasets: bool = False,
-    dataset_class: SynthesizedDataset  = SynthesizedDataset,
+    dataset_class: SynthesizedDataset = SynthesizedDataset,
 ):
     """Construct a dataloader by first constructing datasets from `dataset_kwargs`
     and concatenating them.
@@ -452,12 +587,17 @@ def setup_dataloader(
 
     # Individual datasets
     if separate_datasets:
-        dl = {k: make_dataloader(dataset_class(**kw), **dataloader_kwargs) for k,kw in dataset_config.dataset_kwargs.items()}
+        dl = {
+            k: make_dataloader(dataset_class(**kw), **dataloader_kwargs)
+            for k, kw in dataset_config.dataset_kwargs.items()
+        }
         # original datasets are in
         # dataloader.dataset.dataset.datasets
         #                        subset  concat  list of original ds
     else:
-        dl = make_dataloader(concatenate_datasets(dataset_config, dataset_class), **dataloader_kwargs)
+        dl = make_dataloader(
+            concatenate_datasets(dataset_config, dataset_class), **dataloader_kwargs
+        )
     return dl
 
 
@@ -589,7 +729,6 @@ def write_dataset_subjects(
                     parts += [suffix] if isinstance(suffix, str) else suffix
                 # print(out_dir / (".".join(parts) + ".txt"))
                 np.savetxt(out_dir / (".".join(parts) + ".txt"), subarr, fmt="%s")
-
 
 
 def make_subsets(subs, subsets: dict, rng_seed: int | None = None):
