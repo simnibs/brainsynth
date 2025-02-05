fa6a44e6a48dadd67f83bd08cf587229b81aa357
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2025-02-05 15:03:06 +0100

    update tool to convert vertices pt to fs surface

--- tools/torch_vertices_to_freesurfer_surface.py
+++ tools/torch_vertices_to_freesurfer_surface.py
@@ -4,16 +4,17 @@ import sys
 import nibabel as nib
 import torch
 
-from brainnet.mesh.topology import get_recursively_subdivided_topology
+from brainnet.mesh.topology import FsAverageTopology
+# from brainnet.mesh.topology import StandardTopology
 
 
 if __name__ == "__main__":
     filename = Path(sys.argv[1])
     hemi = sys.argv[2]
+    resolution = int(sys.argv[3])
 
-    resolution = 6
-
-    top = get_recursively_subdivided_topology(resolution)[-1]
+    top = FsAverageTopology.recursive_subdivision(resolution)[-1]
+    # top = StandardTopology.recursive_subdivision(resolution)[-1]
     if hemi == "rh":
         top.reverse_face_orientation()
     faces = top.faces.numpy()
